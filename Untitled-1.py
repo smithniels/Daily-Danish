2c06c7517dc5f128f8fa599d75d80d47530e711c
Niels Smith <30351969+smithniels@users.noreply.github.com>
2022-07-10 20:38:00 +0200

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+
+
+#  <-- GMAIL Code (the internet says that should be in a local environment for safety)
+import os
+username = os.environ.get('USERNAMEEE')
+password = os.environ.get('PASSWORD')
+print(username)
+print(password)
