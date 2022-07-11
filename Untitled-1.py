3ff3d61d31094cd94a6b1ce784795f0cb473f2f0
Niels Smith <30351969+smithniels@users.noreply.github.com>
2022-07-11 09:55:18 +0200

    new day new error

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,30 @@
 import os
+import smtplib
+import ssl
+from email.message import EmailMessage
 
 username=os.environ.get('userUSERuser')
 print(username)
 password=os.environ.get('PASS')
-print(password)
+print(password, username)
+
+email_sender = username
+email_password = password
+email_receiver = username
+
+
+subject = 'this is from Niels to Niels'
+body = """
+Let's try to send an email
+"""
+
+em = EmailMessage()
+em['From'] = username
+em['To'] = username
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',  997 , context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
