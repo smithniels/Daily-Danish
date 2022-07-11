bd09389fe3cde2ca2dd05e112fbac8c5d07b3297
Niels Smith <30351969+smithniels@users.noreply.github.com>
2022-07-11 09:09:14 +0200

    why won't username save :(

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,6 +2,9 @@
 
 #  <-- GMAIL Code (the internet says that should be in a local environment for safety)
 import os
-password = os.environ.get('PASSWORD')
-usernameee = os.environ.get('USERNAMEEE')
-print(usernameee,password)
+
+username=os.environ.get('USERNAME')
+password=os.environ.get('PASSWORD')
+print(username,password)
+
+
