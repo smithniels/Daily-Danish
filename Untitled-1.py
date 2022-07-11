b553b7ab2d11ede95b0fe8492962fd7dcfe877ab
Niels Smith <30351969+smithniels@users.noreply.github.com>
2022-07-11 09:27:41 +0200

    it worked, but i'm not 100% sure why

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,6 @@
-
-
-#  <-- GMAIL Code (the internet says that should be in a local environment for safety)
 import os
 
-username=os.environ.get('USERNAME')
-password=os.environ.get('PASSWORD')
-print(username,password)
-
-
+username=os.environ.get('userUSERuser')
+print(username)
+password=os.environ.get('PASS')
+print(password)
