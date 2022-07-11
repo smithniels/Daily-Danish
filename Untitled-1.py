cabbbe8b84538f28f606edc4ec7d4d328a019f13
Niels Smith <30351969+smithniels@users.noreply.github.com>
2022-07-11 08:48:55 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,7 +2,6 @@
 
 #  <-- GMAIL Code (the internet says that should be in a local environment for safety)
 import os
-username = os.environ.get('USERNAMEEE')
 password = os.environ.get('PASSWORD')
-print(username)
-print(password)
+usernameee = os.environ.get('USERNAMEEE')
+print(usernameee,password)
